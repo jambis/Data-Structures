b5615c2c02c92cd739393f3a62a1fd238ce2aaa0
jambis <imjaamees@gmail.com>
2020-02-26 16:29:02 -0600

    Completed BST with methods

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+# from dll_queue import Queue
+# from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -12,21 +12,55 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
-
+        if value >= self.value:
+            if self.right:
+                return self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+        else:
+            if self.left:
+                return self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+        
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        
+        if target >= self.value and self.right:
+            return self.right.contains(target)
+        elif target < self.value and self.left:
+            return self.left.contains(target)
+        else:
+            return False
+
+
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        if self.right and self.left:
+            cb(self.value)
+            self.right.for_each(cb) 
+            self.left.for_each(cb)
+        elif self.left:
+            cb(self.value)
+            self.left.for_each(cb)
+        elif self.right:
+            cb(self.value)
+            self.right.for_each(cb)
+        else:
+            cb(self.value)
+
 
     # DAY 2 Project -----------------------
 
